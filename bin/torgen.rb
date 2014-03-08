32d8ebe624b5dcfdb8b0b845941ffca7293fe8fc
Amanda Galligan <amanda.galligan@workday.com>
2014-03-08 15:55:39 +0000

    Updating torgen.rb to include colorized comment during output gen

--- bin/torgen.rb
+++ bin/torgen.rb
@@ -18,6 +18,24 @@ def csv_convert(csvfile)
   end
 end
 
+#Define ability to output results in bold/red/green to CLI 
+
+def colorize(text, color_code)
+  " \e[#{color_code}m#{text}\e[0m"
+end
+
+def red(text) 
+    colorize(text, 31) 
+end
+
+def green(text) 
+    colorize(text, 32) 
+end
+
+def bold(text) 
+    colorize(text, 1)
+end
+
  # This hash will hold all of the options
  # parsed from the command-line by
  # OptionParser.
@@ -25,7 +43,7 @@ end
 options = {}
 options[:template] = :tor
 
- optparse = OptionParser.new do |o|
+ optparse = OptionParser.new do |o| 
   o.on('-s', '--silo SILO', "E.g prod.dc1 or nprd.dc1") do |f|
     options[:silo] = f
   end
@@ -52,41 +70,45 @@ begin
     puts "Missing options: #{missing.join(', ')}"                  #
     puts optparse                                                  #
     exit                                                           #
-  end
+end
 #end                                                             #
 rescue OptionParser::InvalidOption, OptionParser::MissingArgument      #
   puts $!.to_s                                                           # Friendly output when parsing fails
-  puts optparse                                                          #
+  puts optparse                                                          # 
   exit                                                                   #
 end  
 
 if (options[:csv])
- $file = (options[:csv])
- csv_convert(options[:csv])
+  $file = (options[:csv])
+  csv_convert(options[:csv])
  
- f = File.open("../yaml/#{options[:silo]}-capadd.yaml","w")
- puts "\t Generating YAML SW source file ../yaml/#{options[:silo]}-capadd.yaml"
- puts root.to_yaml
- f.puts root.to_yaml
- f.close
+  f = File.open("../yaml/#{options[:silo]}-capadd.yaml","w")
+  puts bold("\t Generating YAML SW source file ../yaml/#{options[:silo]}-capadd.yaml")
+  puts green(root.to_yaml)
+  f.puts root.to_yaml
+  f.close
 else
- siloinfo = parse_yaml("../site/#{options[:silo]}.yaml")
- switchinfo = parse_yaml(options[:file])
- hostnames = switchinfo[:hostnames]
- template = ERB.new( File.read("../templates/#{options[:template]}.erb"), nil, '-')
+  siloinfo = parse_yaml("../site/#{options[:silo]}.yaml")
+  switchinfo = parse_yaml(options[:file])
+  hostnames = switchinfo[:hostnames]
+  template = ERB.new( File.read("../templates/#{options[:template]}.erb"), nil, '-')
+   puts red("\t For Silo = #{options[:silo]}") 
+   puts red("\t Using Template = #{options[:template]}") 
+   puts red("\t Using YAML databag = ../yaml/#{options[:silo]}-capadd.yaml \n") 
+
   hostnames.each_key do |host|
-   if File.exists?"../#{options[:silo]}" 
-    File.open "../#{options[:silo]}/#{host}.#{siloinfo[:silo]}.wd.wnaset", "w" do |out| 
-    puts "\t Generating ../#{options[:silo]}/#{host}.#{siloinfo[:silo]}.wd.wnaset"  
+    if File.exists?"../#{options[:silo]}" 
+    File.open "../#{options[:silo]}/#{hostnames[host][:vcp0]}.wnaset", "w" do |out|
+    puts bold("\t Generating ../#{options[:silo]}/#{host}.#{siloinfo[:silo]}.wd.wnaset") 
     out.puts template.result binding
     end
    else
     FileUtils.mkdir_p "../#{options[:silo]}"
     File.open "../#{options[:silo]}/#{host}.#{siloinfo[:silo]}.wd.wnaset", "w" do |out| 
-    puts "\t Generating ../#{options[:silo]}/#{host}.#{siloinfo[:silo]}.wd.wnaset"  
+    puts bold("\t Generating ../#{options[:silo]}/#{host}.#{siloinfo[:silo]}.wd.wnaset") 
     out.puts template.result binding
-    end
-   end      
+      end
+  end      
 end
 end
 
